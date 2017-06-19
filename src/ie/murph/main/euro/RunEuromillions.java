f45590b9eca8f25da540a0d40990a5c0b8de0088
Mur3ph <paulmurphymainstreet@gmail.com>
2017-06-19 23:48:41 +0100

    fortmat

--- src/ie/murph/main/euro/RunEuromillions.java
+++ src/ie/murph/main/euro/RunEuromillions.java
@@ -9,51 +9,48 @@ import ie.murph.main.euro.read.model.numbers.BonusNumbers;
 import ie.murph.main.euro.read.model.numbers.MainNumbers;
 import ie.murph.main.euro.read.statistics.MostFrequentNumbers;
 
-public class RunEuromillions
-{
-    public static void main(String[] args)
-    {
-	RunEuromillions runEuromillions = new RunEuromillions();
-	
-	System.out.println("Reading from random number generation!\n");
-	runEuromillions.runLottoUsingRandomGenerator();
-	
-	System.out.println(" ");
-	
-	System.out.println("Reading from file!\n");
-	runEuromillions.runLottoUsingTextFile();
-    }
-    
-    private void runLottoUsingRandomGenerator()
-    {
-	RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
-	
-	EuromillionsMainNumbers firstFiveEuroNumbers = new EuromillionsMainNumbers(randomNumberGenerator);
-	firstFiveEuroNumbers.selectMainEuroNumbers();
-	firstFiveEuroNumbers.printMainEuroNumbers();
-	
-	System.out.println(" ");
-	EuromillionsBonusNumbers bonusEuroNumbers = new EuromillionsBonusNumbers(randomNumberGenerator);
-	bonusEuroNumbers.selectBonusEuroNumbers();
-	bonusEuroNumbers.printBonusEuroNumbers();
-    }
+public class RunEuromillions {
+	public static void main(String[] args) {
+		RunEuromillions runEuromillions = new RunEuromillions();
 
-    private void runLottoUsingTextFile()
-    {
-	ConvertVariables convertVariables = new ConvertVariables();
-	ReadEuromillionsTextFile readEuromillionsTextFile = new ReadEuromillionsTextFile(convertVariables);
-	readEuromillionsTextFile.readTextFile();
-	MainNumbers mainNumbers = new MainNumbers(readEuromillionsTextFile.getUnsortedMainLottoNumbersFromTextFile());
-	BonusNumbers bonusNumbers = new BonusNumbers(readEuromillionsTextFile.getUnsortedBonusLottoNumbersFromTextFile());
-	
-	MostFrequentNumbers mode = new MostFrequentNumbers();
-	mode.getMostFrequentLottoNumbers(mainNumbers.getMainLottoNumbers(), 5);
-	mode.printMostFrequentLottoNumbers();
-	mode.clearMostFrequentNumbers();
-	
-	System.out.println(" ");
-	mode.getMostFrequentLottoNumbers(bonusNumbers.getBonusLottoNumbers(), 2);
-	mode.printMostFrequentLottoNumbers();
-	mode.clearMostFrequentNumbers();
-    }
+		System.out.println("Reading from random number generation!\n");
+		runEuromillions.runLottoUsingRandomGenerator();
+
+		System.out.println(" ");
+
+		System.out.println("Reading from file!\n");
+		runEuromillions.runLottoUsingTextFile();
+	}
+
+	private void runLottoUsingRandomGenerator() {
+		RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+
+		EuromillionsMainNumbers firstFiveEuroNumbers = new EuromillionsMainNumbers(randomNumberGenerator);
+		firstFiveEuroNumbers.selectMainEuroNumbers();
+		firstFiveEuroNumbers.printMainEuroNumbers();
+
+		System.out.println(" ");
+		EuromillionsBonusNumbers bonusEuroNumbers = new EuromillionsBonusNumbers(randomNumberGenerator);
+		bonusEuroNumbers.selectBonusEuroNumbers();
+		bonusEuroNumbers.printBonusEuroNumbers();
+	}
+
+	private void runLottoUsingTextFile() {
+		ConvertVariables convertVariables = new ConvertVariables();
+		ReadEuromillionsTextFile readEuromillionsTextFile = new ReadEuromillionsTextFile(convertVariables);
+		readEuromillionsTextFile.readTextFile();
+		MainNumbers mainNumbers = new MainNumbers(readEuromillionsTextFile.getUnsortedMainLottoNumbersFromTextFile());
+		BonusNumbers bonusNumbers = new BonusNumbers(
+				readEuromillionsTextFile.getUnsortedBonusLottoNumbersFromTextFile());
+
+		MostFrequentNumbers mode = new MostFrequentNumbers();
+		mode.getMostFrequentLottoNumbers(mainNumbers.getMainLottoNumbers(), 5);
+		mode.printMostFrequentLottoNumbers();
+		mode.clearMostFrequentNumbers();
+
+		System.out.println(" ");
+		mode.getMostFrequentLottoNumbers(bonusNumbers.getBonusLottoNumbers(), 2);
+		mode.printMostFrequentLottoNumbers();
+		mode.clearMostFrequentNumbers();
+	}
 }
