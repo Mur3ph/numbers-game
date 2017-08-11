3fe4fae9a62de6f6e819637b2307dfc12d280db5
Mur3ph <paulmurphymainstreet@gmail.com>
2017-08-11 15:12:42 +0100

    added new file numbers, only winners

--- src/ie/murph/main/euro/read/file/ReadEuromillionsTextFile.java
+++ src/ie/murph/main/euro/read/file/ReadEuromillionsTextFile.java
@@ -8,95 +8,77 @@ import java.util.Scanner;
 
 import ie.murph.main.euro.read.conversion.ConvertVariables;
 
-public class ReadEuromillionsTextFile
-{
-    // 14 columns
-//    private final String DATA_FILE_URL = "data/euro-lotto-numbers.txt";
-    private final String DATA_FILE_URL = "data/euro-lotto-numbers-03june2017.txt";
-    private final int FIRST_FIVE_COLUMNS_OF_TEXT_FILE = 5;
-    private final int LAST_TWO_COLUMNS_OF_TEXT_FILE = 2;
-    private final int FIVE_MAIN_LOTTO_NUMBERS_OF_TEXT_FILE = 5;
-    private final int TWO_BONUS_LOTTO_NUMBERS_OF_TEXT_FILE = 2;
-    private Scanner read;
-    private final List<Integer> mainNumbers;
-    private final List<Integer> bonusNumbers;
-    private final ConvertVariables convertVariables;
-    
-    public ReadEuromillionsTextFile(ConvertVariables convertVariables)
-    {
-	try
-	{
-	    read = new Scanner (new File(DATA_FILE_URL));
-	} 
-	catch (FileNotFoundException e)
-	{
-	    e.printStackTrace();
-	}
-	mainNumbers = new ArrayList<Integer>();
-	bonusNumbers = new ArrayList<Integer>();
-	this.convertVariables = convertVariables;
-    }
-    
-    public void readTextFile()
-    {
-	searchForComa();
-		
-	while(isStillDataInTextFile())
-	{
-	   skip(FIRST_FIVE_COLUMNS_OF_TEXT_FILE);
-	   addMainEuroLottoNumbers();
-	   addBonusEuroLottoNumbers();
-	   skip(LAST_TWO_COLUMNS_OF_TEXT_FILE);
-	}
-	closeScanner();
-    }
+public class ReadEuromillionsTextFile {
+	// 14 columns
+	// private final String DATA_FILE_URL = "data/euro-lotto-numbers.txt";
+	private final String DATA_FILE_URL = "data/euro-lotto-numbers-11Aug2017.txt";
+	private final int FIRST_FIVE_COLUMNS_OF_TEXT_FILE = 5;
+	private final int LAST_TWO_COLUMNS_OF_TEXT_FILE = 2;
+	private final int FIVE_MAIN_LOTTO_NUMBERS_OF_TEXT_FILE = 5;
+	private final int TWO_BONUS_LOTTO_NUMBERS_OF_TEXT_FILE = 2;
+	private Scanner read;
+	private final List<Integer> mainNumbers;
+	private final List<Integer> bonusNumbers;
+	private final ConvertVariables convertVariables;
 
-    private void searchForComa()
-    {
-	read.useDelimiter(",");
-    }
-    
-    private boolean isStillDataInTextFile()
-    {
-	return read.hasNext();
-    }
-    
-    private void skip(int numberOfRowsToSkip)
-    {
-	for(int iteration = 0; iteration < numberOfRowsToSkip; iteration++)
-	{
-	    read.next();
+	public ReadEuromillionsTextFile(ConvertVariables convertVariables) {
+		try {
+			read = new Scanner(new File(DATA_FILE_URL));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		mainNumbers = new ArrayList<Integer>();
+		bonusNumbers = new ArrayList<Integer>();
+		this.convertVariables = convertVariables;
 	}
-    }
-    
-    private void addMainEuroLottoNumbers()
-    {
-	for(int iteration = 0; iteration < FIVE_MAIN_LOTTO_NUMBERS_OF_TEXT_FILE; iteration++)
-	{
-	    mainNumbers.add(convertVariables.convertStringToInteger(read.next()));
+
+	public void readTextFile() {
+		searchForComa();
+
+		while (isStillDataInTextFile()) {
+			skip(FIRST_FIVE_COLUMNS_OF_TEXT_FILE);
+			addMainEuroLottoNumbers();
+			addBonusEuroLottoNumbers();
+			skip(LAST_TWO_COLUMNS_OF_TEXT_FILE);
+		}
+		closeScanner();
 	}
-    }
-    
-    private void addBonusEuroLottoNumbers()
-    {
-	for(int iteration = 0; iteration < TWO_BONUS_LOTTO_NUMBERS_OF_TEXT_FILE; iteration++)
-	{
-	    bonusNumbers.add(convertVariables.convertStringToInteger(read.next()));
+
+	private void searchForComa() {
+		read.useDelimiter(",");
 	}
-    }
-    
-    private void closeScanner()
-    {
-	read.close();
-    }
-    
-    public List<Integer> getUnsortedMainLottoNumbersFromTextFile()
-    {
-	return this.mainNumbers;
-    }
-    
-    public List<Integer> getUnsortedBonusLottoNumbersFromTextFile()
-    {
-	return this.bonusNumbers;
-    }
+
+	private boolean isStillDataInTextFile() {
+		return read.hasNext();
+	}
+
+	private void skip(int numberOfRowsToSkip) {
+		for (int iteration = 0; iteration < numberOfRowsToSkip; iteration++) {
+			read.next();
+		}
+	}
+
+	private void addMainEuroLottoNumbers() {
+		for (int iteration = 0; iteration < FIVE_MAIN_LOTTO_NUMBERS_OF_TEXT_FILE; iteration++) {
+			mainNumbers.add(convertVariables.convertStringToInteger(read.next()));
+		}
+	}
+
+	private void addBonusEuroLottoNumbers() {
+		for (int iteration = 0; iteration < TWO_BONUS_LOTTO_NUMBERS_OF_TEXT_FILE; iteration++) {
+			bonusNumbers.add(convertVariables.convertStringToInteger(read.next()));
+		}
+	}
+
+	private void closeScanner() {
+		read.close();
+	}
+
+	public List<Integer> getUnsortedMainLottoNumbersFromTextFile() {
+		return this.mainNumbers;
+	}
+
+	public List<Integer> getUnsortedBonusLottoNumbersFromTextFile() {
+		return this.bonusNumbers;
+	}
 }
